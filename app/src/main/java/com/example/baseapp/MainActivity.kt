48553d0c7e8fa6d6ffd5a2e9e321c356349a8a6d
jrg <punguino@gmail.com>
2023-07-13 15:14:30 -0700

    first commit

--- app/src/main/java/com/example/baseapp/MainActivity.kt
+++ app/src/main/java/com/example/baseapp/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BaseAppTheme {
-        Greeting("Andrea")
+        Greeting("Andrea ")
     }
 }
